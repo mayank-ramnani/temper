125377ac7294ec89ab30d1291fa38f65afcae48f
mayank-ramnani <mayankr99@gmail.com>
2024-03-25 21:04:14 -0400

    add printing default options and compiler

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import argparse
 import re
 import subprocess
 
+# gets output from stderr if stdout is empty
+# `gcc -v` writes output to stderr
+# output is split into a list of lines
 def run_shell_command(command):
     try:
         # Run the command in the shell
@@ -10,10 +13,13 @@ def run_shell_command(command):
         # Check if the command was successful
         if result.returncode == 0:
             # Return the output
-            return result.stdout.strip()
+            if result.stdout:
+                return result.stdout.strip().split('\n')
+            else:
+                return result.stderr.strip().split('\n')
         else:
             # If the command failed, print error message
-            print("Error:", result.stderr.strip())
+            print("Error:", result.stderr.strip().split('\n'))
             return None
     except Exception as e:
         print("Error:", e)
@@ -38,14 +44,27 @@ def extract_compiler_options(makefile_path):
 
     return compiler_options
 
-def main():
-    # Argument parser setup
-    parser = argparse.ArgumentParser(description='Extract enabled compiler options from a Makefile')
-    parser.add_argument('--makefile', help='Path to the Makefile', required=True)
-    args = parser.parse_args()
+def get_compiler():
+    output = run_shell_command("gcc -v")
+    if output is not None:
+        return output[0]
 
+def get_compiler_default_opts():
+    compiler_opts = []
+    build_command = "gcc -v hello.c -o hello"
+    output = run_shell_command(build_command)
+    if output is not None:
+        remove_command = "rm -f hello"
+        run_shell_command(remove_command)
+        opts = output[4].split()
+        for opt in opts:
+            if opt.startswith('-'):
+                compiler_opts.append(opt)
+        return compiler_opts
+        
+def get_makefile_opts(makefile):
     # Extract compiler options
-    enabled_options = extract_compiler_options(args.makefile)
+    enabled_options = extract_compiler_options(makefile)
 
     # Print the enabled compiler options
     print("Enabled Compiler Options:")
@@ -54,13 +73,20 @@ def main():
     # gcc -v hello.c -o hello; rm -f hello
     for option in enabled_options:
         print(option)
+    print(get_compiler_default_opts())
+
+
+def main():
+    # Argument parser setup
+    parser = argparse.ArgumentParser(description='Extract enabled compiler options from a Makefile')
+    parser.add_argument('--compiler', help='Print compiler', required=False)
+    parser.add_argument('--default-opts', help='Print compiler default options', required=False)
+    parser.add_argument('--makefile', help='Path to the Makefile', required=True)
+    args = parser.parse_args()
+
+    get_makefile_opts(args.makefile)
 
 if __name__ == "__main__":
-    build_command = "gcc -v hello.c -o hello"
-    remove_command = "rm -f hello"
-    output = run_shell_command(build_command)
-    if output is not None:
-        print(output)
+    main()
 
-    run_shell_command(remove_command)
-    # main()
+# TODO: check if it starts with '-' each compiler option
